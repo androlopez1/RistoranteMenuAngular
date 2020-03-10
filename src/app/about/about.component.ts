493b2d193fb65228aa6e4776756aaedc24450950
System Administrator <root@Juans-MacBook-Pro.local>
2020-03-10 17:02:46 -0500

    About section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  
+  leader: Leader;
+  leaders: Leader[];
 
-  constructor() { }
+  constructor(private leaderService: LeaderService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit(): void {
+  	let id = this.route.snapshot.params['id'];
+  	this.leader = this.leaderService.getLeader(id);
+  	this.leaders = this.leaderService.getLeaders();
+  }
+
+  goBack(): void {
+  	this.location.back();
   }
 
 }
